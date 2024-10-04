acf7f344eb74c5b9f7786cbcff5ea8e7ee998685
Shreya <a.shreya0806@gmail.com>
2024-10-04 21:10:12 +0530

    minor change

--- client/src/components/LifeScienceMode.js
+++ client/src/components/LifeScienceMode.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import './LifeScienceMode.css';
 import CustomCursor from './CustomCursor';
 import Stars from './Stars'; 
+import { Link } from 'react-router-dom';
+import Microbiology from '../assets/Microbiology.png'
 
 const imageUrls = [
   require('../assets/welcome/tree.png'),
@@ -17,8 +19,6 @@ const loadImages = async (urls) => {
     });
   }));
 };
-
-
 const LifeScienceMode = () => {
   const [images, setImages] = useState([]);
 
@@ -28,14 +28,19 @@ const LifeScienceMode = () => {
     }).catch(error => console.error(error));
   }, []);
   return (
-    <div className="life-science-content">
+    <div>
     <CustomCursor />
     <Stars 
         images={images} 
         numStars={80} 
         canvasClass="home-canvas" 
         linkersColor="rgba(0, 0, 0, 0.3)" 
-      />     
+      />   
+      <div className="image-container">
+      <Link to="/welcome" aria-label="MicroBio Page" >
+  <img src={Microbiology} alt="Life Science Mode: Link to Welcome Page" loading="lazy" />
+</Link>
+    </div>  
     </div>
   );
 };
